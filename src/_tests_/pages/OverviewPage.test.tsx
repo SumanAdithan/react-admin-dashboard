79b7e2c92a45c8215e29d4768240e864126fd124
SumanAdithan <sumanadithang34@gmail.com>
2024-12-10 21:57:01 +0530

    test: add test case to verify chart titles for overview Page

--- src/_tests_/pages/OverviewPage.test.tsx
+++ src/_tests_/pages/OverviewPage.test.tsx
@@ -1,3 +1,4 @@
+import { CategoryDistributionChart, SalesChannelChart, SalesOverviewChart } from '@components';
 import { OverviewPage } from '@pages';
 import { render, screen } from '@testing-library/react';
 import { it } from 'vitest';
@@ -10,5 +11,20 @@ describe('status cards', () => {
         expect(screen.getByText('New Users')).toBeInTheDocument();
         expect(screen.getByText('Total Products')).toBeInTheDocument();
         expect(screen.getByText('Conversion Rate')).toBeInTheDocument();
+    });
+});
+
+describe('charts', () => {
+    it('should render line chart with title', () => {
+        render(<SalesOverviewChart />);
+        expect(screen.getByText('Sales Overview')).toBeInTheDocument();
+    });
+    it('should render pie chart with title', () => {
+        render(<CategoryDistributionChart />);
+        expect(screen.getByText('Category Distribution')).toBeInTheDocument();
+    });
+    it('should render bar chart with title', () => {
+        render(<SalesChannelChart />);
+        expect(screen.getByText('Sales By Channel')).toBeInTheDocument();
     });
 });
